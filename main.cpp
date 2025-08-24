ba9ccfd3e44fa47f97056da6305c1fce3b0eaeed
Sahil <sahilbasia8@gmail.com>
2025-08-24 12:13:13 +0530

    Cmake for building the project added

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "./header_files/BUS.h"
+#include "./header_files/DRAM.h"
+
+#include <iostream>
+
+int main(){
+    std::cout<<"RISC V emulation working"<<std::endl;
+    BUS bus;
+    // store_on_bus(&bus, 0x8, 32, 1);
+    return 0;
+}
