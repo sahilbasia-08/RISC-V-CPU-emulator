2eec2b1ad8d64b419af31dc36d0d672de8c6f567
Sahil <sahilbasia8@gmail.com>
2025-08-29 15:35:29 +0530

    tested with some unit test 2

--- main.cpp
+++ main.cpp
@@ -5,6 +5,23 @@
 
 #include <iostream>
 
+static void test_decode_word(CPU* cpu, uint32_t addr, const char* label) {
+    uint32_t inst = (uint32_t)cpu_load(cpu, addr, 32);  // read back 32-bit word
+    std::cout << label << " @0x" << std::hex << addr << " inst=0x" << inst << std::dec << "\n";
+    std::cout << "  opcode=" << opcode(inst)
+              << " rd="     << rd(inst)
+              << " funct3=" << func3(inst)
+              << " rs1="    << rs1(inst)
+              << " rs2="    << rs2(inst)
+              << " funct7=" << func7(inst) << "\n";
+
+    // Try each immediate; print what makes sense for that format
+    std::cout << "  imm_i=" << imm_i(inst)
+              << "  imm_s=" << imm_s(inst)
+              << "  imm_j=" << imm_j(inst) << "\n\n";
+}
+
+
 int main(){
     std::cout<<"RISC V emulation working"<<std::endl;
     // BUS bus;
@@ -16,11 +33,26 @@ int main(){
     std::cout<<" -> Dumping register content of the current state of the CPU"<<std::endl;
     dump_register_content(&cpu);
     std::cout<<"\n";
-    std::cout<<" -> Testing cpu load and store to some x address"<<std::endl;
-    std::cout<<"Storing value 10919191919 for testing at address 0x80000000"<<std::endl;
-    cpu_store(&cpu, 2147483648, 64, (uint64_t)10919191919);
-    std::cout<<"value loaded is "<<cpu_load(&cpu, 2147483648, 64)<<std::endl;
+    // std::cout<<" -> Testing cpu load and store to some x address"<<std::endl;
+    // std::cout<<"Storing value 10919191919 for testing at address 0x80000000"<<std::endl;
+    // cpu_store(&cpu, 2147483648, 64, (uint64_t)10919191919);
+    // std::cout<<"value loaded is "<<cpu_load(&cpu, 2147483648, 64)<<std::endl;
 
-    imm_s(2147483648);
+    // imm_s(2147483648);
+
+
+    uint32_t inst_addi = 0xFFF00093; // ADDI x1,x0,-1
+    uint32_t inst_sw   = 0xFE552823; // SW x5,-16(x10)
+    uint32_t inst_jalP = 0x010000EF; // JAL x1,+16
+    uint32_t inst_jalN = 0xFFDFF0EF; // JAL x1,-4
+
+    cpu_store(&cpu, DRAM_BASE_ADDRESS + 0x00, 32, inst_addi);
+    cpu_store(&cpu, DRAM_BASE_ADDRESS + 0x04, 32, inst_sw);
+    cpu_store(&cpu, DRAM_BASE_ADDRESS + 0x08, 32, inst_jalP);
+    cpu_store(&cpu, DRAM_BASE_ADDRESS + 0x0C, 32, inst_jalN);
+    test_decode_word(&cpu, DRAM_BASE_ADDRESS + 0x00, "ADDI x1,x0,-1");
+    test_decode_word(&cpu, DRAM_BASE_ADDRESS + 0x04, "SW   x5,-16(x10)");
+    test_decode_word(&cpu, DRAM_BASE_ADDRESS + 0x08, "JAL  x1,+16");
+    test_decode_word(&cpu, DRAM_BASE_ADDRESS + 0x0C, "JAL  x1,-4");
     return 0;
 }
--- src/INSTRUCTION.cpp
+++ src/INSTRUCTION.cpp
@@ -2,57 +2,96 @@
 
 #include <iostream>
 
+// Currently we programming for the 32 bit wide instruction lenght
+// and thereby emulating for RV32 architecture
+
 // returning the LSB 7 bit, from the instruction at addrress addr in DRAM
-// LSB 7 bits contains the opsocde basically
-uint64_t opcode(uint32_t addr){
-    return (addr & 0x7F);
+// LSB 7 bits contains the opcode basically
+uint64_t opcode(uint32_t instrn){
+    return (instrn & 0x7F);
 }
 
 // rd takes 5 bits in 32 bit wide instrunction, 
 // and ranges from 7-11 bit range, so to get those 5 bits
 // we are using 0x1F<<7 :- basically shifting the 0x0001 1111 to left 7 times 
 // to get those 5 bits from 7-11 range in 32 bit 
-uint64_t rd(uint32_t addr){
-    return (0x1F & (addr>>7));
+uint64_t rd(uint32_t instrn){
+    return (0x1F & (instrn>>7));
 }
 
 // func3 take 3 bits from range 12-14
-uint64_t func3(uint32_t addr){
-    return (0x7 & (addr>>12));
+uint64_t func3(uint32_t instrn){
+    return (0x7 & (instrn>>12));
 }
 
 // rs1 takes 5 bits from rnage 15- 19
-uint64_t rs1(uint32_t addr){
-    return (0x1F & (addr>>15));
+uint64_t rs1(uint32_t instrn){
+    return (0x1F & (instrn>>15));
 }
 
 // rs2 takes 5 bits from range 20 - 24
-uint64_t rs2(uint32_t addr){
-    return (0x1F & (addr>>20));
+uint64_t rs2(uint32_t instrn){
+    return (0x1F & (instrn>>20));
 }
 
 // func7 takes 7 bits from range 25-31
-uint64_t func7(uint32_t addr){
-    return (0x7F & (addr>>25));
+uint64_t func7(uint32_t instrn){
+    return (0x7F & (instrn>>25));
 }
 
 // returns the last 22 bits of the 32 bit wide I-type insstruction
 
 // immediate_i type are signed mode, so we have to maintian the 
 // sign bit. If negative number is present we have to ensure the MSB is 1 
-int64_t imm_i(uint32_t addr){
-    int64_t temp = (addr>>20);
+int64_t imm_i(uint32_t instrn){
+    int64_t temp = (instrn>>20);
     int64_t shift = 64 - 12;
     temp = (temp << shift) >> shift;
     return temp;
 }
 
-// immedetea_s type is also a sigend immediate mode
-int64_t imm_s(uint32_t addr) {
-   int64_t temp = (addr >> 7) & 0x1F;
-   temp = ((((addr >> 25) & 0x7F) << 5) | temp);
+// immedetea_s type is also a sigend immediate mode, so we are keeping 
+// using a sign extension mechanism to maintain the sign bit
+int64_t imm_s(uint32_t instrn) {
+   int64_t temp = (instrn >> 7) & 0x1F;
+   temp = ((((instrn >> 25) & 0x7F) << 5) | temp);
 
    int64_t shift = 64 - 12;
    temp = (temp << shift) >> shift;
    return temp;
+}
+// for immediate in J type, we have this 
+// imm[20]   = instrn[31]
+// imm[10:1] = instrn[30:21]
+// imm[11]   = instrn[20]
+// imm[19:12]= instrn[19:12]
+
+int64_t imm_j(uint32_t instrn){
+    int64_t temp = 0;
+    temp |= ((instrn >> 21) & 0x3FF ) <<1;// this is for imm[10:1]
+    temp |= ((instrn >> 20) & 0x1 ) << 11; // this is for imm[11]
+    temp |= ((instrn >> 12) & 0xFF ) << 12; // this is for imm[19:12]
+    temp |= ((instrn >> 31) & 0x1) << 20; // this is for imm[20]
+
+    // performing sign extension, as this immediates can be sign value also
+    int64_t shift = 64 - 21;
+    temp = (temp << shift) >> shift;
+
+    return temp;
+}
+
+// for immediate in U type 
+// imm[31:12] = 
+uint64_t imm_u(uint32_t instrn){
+    return (instrn & 0xFFFFF999);
+}
+
+int64_t imm_b(uint32_t instrn){
+    int64_t temp = 0;
+    temp |= (instrn & 0x80000000) >> 19;
+    temp |= ((instrn & 0x80) << 4);
+    temp |= ((instrn >> 20) & 0x7e0);
+    temp |= ((instrn >> 7) & 0x1e);
+
+    return ((int64_t)(int32_t)instrn);
 }
